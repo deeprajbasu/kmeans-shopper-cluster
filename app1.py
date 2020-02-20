37c012a654ea9479f10d9bc1b93be973601ac12a
deeprajbasu <deepraj.basu@gmail.com>
2020-02-20 15:53:41 +0530

    first

---
+++ app1.py
@@ -0,0 +1,43 @@
+
+# importing the necessary dependencies
+from flask import Flask, render_template, request,jsonify
+from flask_cors import CORS,cross_origin
+import numpy as np
+import pandas as pd
+import pickle
+#import pandas as pd
+
+app = Flask(__name__) # initializing a flask app
+
+@app.route('/',methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
+    return render_template("index.html")
+
+@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
+@cross_origin()
+def index():
+    if request.method == 'POST':
+        try:
+            # #  reading the inputs given by the user
+
+
+
+
+
+
+            return render_template('results.html',prediction=result)
+
+
+        except Exception as e:
+            print('The Exception message is: ',e)
+            return 'something is wrong'
+    # return render_template('results.html')
+    else:
+        return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    #app.run(host='127.0.0.1', port=8001, debug=True)
+	app.run(debug=True) # running the app
